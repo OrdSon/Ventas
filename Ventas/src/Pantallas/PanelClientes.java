fbf2657644f1f1cb096f96061d1563f41627ddfa
OrdSon201931185 <anthonyordonez201931185@cunoc.edu.gt>
2021-07-08 15:54:02 -0600

    Volvemos a trabajar

--- Ventas/src/Pantallas/PanelClientes.java
+++ Ventas/src/Pantallas/PanelClientes.java
@@ -37,21 +37,8 @@ public class PanelClientes extends javax.swing.JPanel {
         java.awt.GridBagConstraints gridBagConstraints;
 
         PanelOpcionesBusqueda = new javax.swing.JPanel();
-        PanelBuscarCodigo = new javax.swing.JPanel();
-        lblBuscarCodigo = new javax.swing.JLabel();
-        txtBuscarCodigo = new javax.swing.JTextField();
-        txtBuscarCodigo1 = new javax.swing.JTextField();
-        PanelBuscarPorTienda1 = new javax.swing.JPanel();
-        lblBuscarPorTienda2 = new javax.swing.JLabel();
-        jButton4 = new javax.swing.JButton();
-        lblBuscarPorTienda3 = new javax.swing.JLabel();
-        lblBuscarPorTienda5 = new javax.swing.JLabel();
-        lblBuscarPorTienda6 = new javax.swing.JLabel();
-        lblBuscarPorTienda7 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
         PanelBaseTabla = new javax.swing.JPanel();
         jScrollDeTabla = new javax.swing.JScrollPane();
         TablaClientes = new javax.swing.JTable();
@@ -69,221 +56,46 @@ public class PanelClientes extends javax.swing.JPanel {
         PanelOpcionesBusqueda.setBackground(new java.awt.Color(153, 184, 211));
         PanelOpcionesBusqueda.setMinimumSize(new java.awt.Dimension(800, 100));
         PanelOpcionesBusqueda.setPreferredSize(new java.awt.Dimension(1467, 100));
-        PanelOpcionesBusqueda.setLayout(new java.awt.GridBagLayout());
 
-        PanelBuscarCodigo.setBackground(new java.awt.Color(144, 173, 198));
-        PanelBuscarCodigo.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                PanelBuscarCodigoMouseExited(evt);
-            }
-        });
-
-        lblBuscarCodigo.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarCodigo.setText("BUSCAR CLIENTE");
-
-        txtBuscarCodigo.setFont(new java.awt.Font("Tw Cen MT", 0, 18)); // NOI18N
-        txtBuscarCodigo.setForeground(new java.awt.Color(204, 204, 204));
-        txtBuscarCodigo.setText("NIT");
-        txtBuscarCodigo.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                txtBuscarCodigoMouseClicked(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                txtBuscarCodigoMouseExited(evt);
-            }
-        });
-
-        txtBuscarCodigo1.setFont(new java.awt.Font("Tw Cen MT", 0, 18)); // NOI18N
-        txtBuscarCodigo1.setForeground(new java.awt.Color(204, 204, 204));
-        txtBuscarCodigo1.setText("NIT");
-        txtBuscarCodigo1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                txtBuscarCodigo1MouseClicked(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                txtBuscarCodigo1MouseExited(evt);
-            }
-        });
-        txtBuscarCodigo1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtBuscarCodigo1ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout PanelBuscarCodigoLayout = new javax.swing.GroupLayout(PanelBuscarCodigo);
-        PanelBuscarCodigo.setLayout(PanelBuscarCodigoLayout);
-        PanelBuscarCodigoLayout.setHorizontalGroup(
-            PanelBuscarCodigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PanelBuscarCodigoLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(PanelBuscarCodigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblBuscarCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(PanelBuscarCodigoLayout.createSequentialGroup()
-                        .addGap(10, 10, 10)
-                        .addGroup(PanelBuscarCodigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtBuscarCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtBuscarCodigo1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(29, Short.MAX_VALUE))
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 252, Short.MAX_VALUE)
         );
-        PanelBuscarCodigoLayout.setVerticalGroup(
-            PanelBuscarCodigoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(PanelBuscarCodigoLayout.createSequentialGroup()
-                .addGap(3, 3, 3)
-                .addComponent(lblBuscarCodigo)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jPanel3.setBackground(new java.awt.Color(204, 255, 204));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 252, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout PanelOpcionesBusquedaLayout = new javax.swing.GroupLayout(PanelOpcionesBusqueda);
+        PanelOpcionesBusqueda.setLayout(PanelOpcionesBusquedaLayout);
+        PanelOpcionesBusquedaLayout.setHorizontalGroup(
+            PanelOpcionesBusquedaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PanelOpcionesBusquedaLayout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtBuscarCodigo1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(txtBuscarCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 500, Short.MAX_VALUE))
         );
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.ipadx = 19;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        PanelOpcionesBusqueda.add(PanelBuscarCodigo, gridBagConstraints);
-
-        PanelBuscarPorTienda1.setBackground(new java.awt.Color(153, 184, 211));
-        PanelBuscarPorTienda1.setMinimumSize(new java.awt.Dimension(180, 100));
-        PanelBuscarPorTienda1.setPreferredSize(new java.awt.Dimension(180, 99));
-        PanelBuscarPorTienda1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                PanelBuscarPorTienda1MouseExited(evt);
-            }
-        });
-        PanelBuscarPorTienda1.setLayout(new java.awt.GridBagLayout());
-
-        lblBuscarPorTienda2.setBackground(new java.awt.Color(255, 255, 255));
-        lblBuscarPorTienda2.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarPorTienda2.setForeground(new java.awt.Color(204, 255, 255));
-        lblBuscarPorTienda2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBuscarPorTienda2.setText("EDITAR CLIENTE");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.gridheight = 3;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(28, 10, 0, 0);
-        PanelBuscarPorTienda1.add(lblBuscarPorTienda2, gridBagConstraints);
-
-        jButton4.setFont(new java.awt.Font("Tw Cen MT Condensed", 1, 18)); // NOI18N
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Recursos/user-check.png"))); // NOI18N
-        jButton4.setText("EDITAR");
-        jButton4.setToolTipText("Crear una nueva venta");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 9;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridheight = 6;
-        gridBagConstraints.ipady = 1;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(29, 18, 0, 20);
-        PanelBuscarPorTienda1.add(jButton4, gridBagConstraints);
-
-        lblBuscarPorTienda3.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarPorTienda3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBuscarPorTienda3.setText("NIT");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridheight = 2;
-        gridBagConstraints.ipady = 1;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(6, 10, 8, 0);
-        PanelBuscarPorTienda1.add(lblBuscarPorTienda3, gridBagConstraints);
-
-        lblBuscarPorTienda5.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarPorTienda5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBuscarPorTienda5.setText("NOMBRE");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 4;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridheight = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(13, 26, 0, 0);
-        PanelBuscarPorTienda1.add(lblBuscarPorTienda5, gridBagConstraints);
-
-        lblBuscarPorTienda6.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarPorTienda6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBuscarPorTienda6.setText("DIRECCIÓN");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 4;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 3;
-        gridBagConstraints.gridheight = 4;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(6, 26, 0, 0);
-        PanelBuscarPorTienda1.add(lblBuscarPorTienda6, gridBagConstraints);
-
-        lblBuscarPorTienda7.setFont(new java.awt.Font("Tw Cen MT Condensed", 0, 20)); // NOI18N
-        lblBuscarPorTienda7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBuscarPorTienda7.setText("TELEFONO");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 4;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.gridheight = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(7, 26, 8, 0);
-        PanelBuscarPorTienda1.add(lblBuscarPorTienda7, gridBagConstraints);
-
-        jTextField2.setFont(new java.awt.Font("Tw Cen MT", 0, 20)); // NOI18N
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 7;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.ipadx = 173;
-        gridBagConstraints.ipady = -6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(11, 4, 0, 0);
-        PanelBuscarPorTienda1.add(jTextField2, gridBagConstraints);
-
-        jTextField3.setFont(new java.awt.Font("Tw Cen MT", 0, 20)); // NOI18N
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 7;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.gridheight = 4;
-        gridBagConstraints.ipadx = 173;
-        gridBagConstraints.ipady = -6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
-        PanelBuscarPorTienda1.add(jTextField3, gridBagConstraints);
-
-        jTextField4.setFont(new java.awt.Font("Tw Cen MT", 0, 20)); // NOI18N
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 7;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridheight = 2;
-        gridBagConstraints.ipadx = 173;
-        gridBagConstraints.ipady = -6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(6, 4, 8, 0);
-        PanelBuscarPorTienda1.add(jTextField4, gridBagConstraints);
-
-        jLabel1.setFont(new java.awt.Font("Tw Cen MT", 0, 20)); // NOI18N
-        jLabel1.setText("34041239");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridwidth = 3;
-        gridBagConstraints.ipadx = 13;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(5, 18, 0, 0);
-        PanelBuscarPorTienda1.add(jLabel1, gridBagConstraints);
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.ipadx = 386;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(2, 97, 0, 98);
-        PanelOpcionesBusqueda.add(PanelBuscarPorTienda1, gridBagConstraints);
+        PanelOpcionesBusquedaLayout.setVerticalGroup(
+            PanelOpcionesBusquedaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
         add(PanelOpcionesBusqueda, java.awt.BorderLayout.PAGE_START);
 
@@ -354,62 +166,18 @@ public class PanelClientes extends javax.swing.JPanel {
 
         add(PanelBaseTabla, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
-    //Metodos que se activan al dar click
-    private void txtBuscarCodigoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtBuscarCodigoMouseClicked
-  
-    }//GEN-LAST:event_txtBuscarCodigoMouseClicked
-    //Metodos que se activan al salir de un textField
-    private void txtBuscarCodigoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtBuscarCodigoMouseExited
-        
-    }//GEN-LAST:event_txtBuscarCodigoMouseExited
-
-    private void PanelBuscarCodigoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PanelBuscarCodigoMouseExited
-    }//GEN-LAST:event_PanelBuscarCodigoMouseExited
-
-    private void PanelBuscarPorTienda1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PanelBuscarPorTienda1MouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_PanelBuscarPorTienda1MouseExited
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void txtBuscarCodigo1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtBuscarCodigo1MouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtBuscarCodigo1MouseClicked
-
-    private void txtBuscarCodigo1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtBuscarCodigo1MouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtBuscarCodigo1MouseExited
-
-    private void txtBuscarCodigo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBuscarCodigo1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtBuscarCodigo1ActionPerformed
 
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel PanelBaseTabla;
-    private javax.swing.JPanel PanelBuscarCodigo;
-    private javax.swing.JPanel PanelBuscarPorTienda1;
     private javax.swing.JPanel PanelOpcionesBusqueda;
     private javax.swing.JTable TablaClientes;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollDeTabla;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JLabel lblBuscarCodigo;
-    private javax.swing.JLabel lblBuscarPorTienda2;
-    private javax.swing.JLabel lblBuscarPorTienda3;
-    private javax.swing.JLabel lblBuscarPorTienda5;
-    private javax.swing.JLabel lblBuscarPorTienda6;
-    private javax.swing.JLabel lblBuscarPorTienda7;
-    private javax.swing.JTextField txtBuscarCodigo;
-    private javax.swing.JTextField txtBuscarCodigo1;
     // End of variables declaration//GEN-END:variables
 }
